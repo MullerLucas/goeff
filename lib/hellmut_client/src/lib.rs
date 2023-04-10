c749be1ae440af4878f4d98ca16856afde34a522
helloki <helloki@mailbox.org>
2023-04-10 19:22:49 +0200

    initial commit

---
+++ lib/hellmut_client/src/lib.rs
@@ -0,0 +1,36 @@
+mod view;
+
+
+use hell_web::{view::{Element, Runtime}, console_log};
+use wasm_bindgen::prelude::*;
+
+
+#[wasm_bindgen(start)]
+pub async fn run() -> Result<(), JsValue> {
+    hell_web::logging::init_logging();
+
+    let rt = Runtime::new();
+    let mut body: Element = rt
+        .document()
+        .body().expect("document should have a body")
+        .try_into().unwrap();
+
+    let test = rt.create_signal(69);
+    console_log!("TESTA: {:?}", test.get());
+    console_log!("TEST: {:?}", rt);
+
+    wait_for_end_of_universe().await.unwrap();
+    console_log!("exit...");
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn version_number() -> String {
+    "0.1".to_string()
+}
+
+fn wait_for_end_of_universe() -> wasm_bindgen_futures::JsFuture {
+    let promise = js_sys::Promise::new(&mut |_resolve, _reject| { });
+    wasm_bindgen_futures::JsFuture::from(promise)
+}
