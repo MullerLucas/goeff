153aef0f0b984909a4c0e570a9f61ec7921799da
helloki <helloki@mailbox.org>
2023-04-16 22:28:11 +0200

    feat (client): implement different element types

--- lib/hellm_web_client/src/lib.rs
+++ lib/hellm_web_client/src/lib.rs
@@ -2,7 +2,7 @@ mod view;
 
 
 use hell_core::error::HellResult;
-use hell_utils_web::view::{Context, Element};
+use hell_utils_web::view::{Context, Element, ElementContainer};
 use wasm_bindgen::prelude::*;
 
 
@@ -24,10 +24,7 @@ async fn run_hell() -> HellResult<()> {
     hell_utils_web::logging::init_logging();
 
     let cx = Context::new();
-    let (mut body, _) = Element::try_from_html(
-        cx,
-        cx.document().body().expect("expected there to be a body")
-    )?;
+    let (mut body, _) = Element::create_body(cx)?;
 
     let chat = view::chat::create_chat(cx)?;
     body.append_child(&chat)?;
--- lib/hellm_web_client/src/view/chat.rs
+++ lib/hellm_web_client/src/view/chat.rs
@@ -1,5 +1,5 @@
 use hell_core::error::HellResult;
-use hell_utils_web::{view::{Element, Context}, console_info};
+use hell_utils_web::{view::{Element, Context, ElementContainer}, console_info};
 
 pub fn create_chat(cx: Context) -> HellResult<Element> {
     let (mut chat, _) = Element::create_div(cx)?;
@@ -12,9 +12,9 @@ pub fn create_chat(cx: Context) -> HellResult<Element> {
     input_box.add_class("chat_input_box")?;
     chat.append_child(&input_box)?;
 
-    let (mut input_field, _) = Element::create_paragraph(cx)?;
+    let (mut input_field, input_field_h) = Element::create_input(cx)?;
     input_field.add_class("chat_input_field")?;
-    input_field.set_text_content(Some("Default Text ..."));
+    input_field.set_attribute("type", "text")?;
     input_box.append_child(&input_field)?;
 
     let (mut input_send_btn, _) = Element::create_button(cx)?;
@@ -23,8 +23,11 @@ pub fn create_chat(cx: Context) -> HellResult<Element> {
     input_box.append_child(&input_send_btn)?;
 
     input_send_btn.add_event_listener("click", move || {
-        let mut output_box = output_box_h.get();
-        let msg = create_chat_msg(cx).expect("failed to create chat-msg");
+        let mut output_box = output_box_h.get_element();
+        let input_field = input_field_h.get_input();
+
+        let txt = input_field.value();
+        let msg = create_chat_msg(cx, &txt).expect("failed to create chat-msg");
         output_box.append_child(&msg).expect("failed to append chat-msg");
         console_info!("send button clicked ...");
     })?;
@@ -32,9 +35,9 @@ pub fn create_chat(cx: Context) -> HellResult<Element> {
     Ok(chat)
 }
 
-pub fn create_chat_msg(cx: Context) -> HellResult<Element> {
+pub fn create_chat_msg(cx: Context, txt: &str) -> HellResult<Element> {
     let (mut msg, _) = Element::create_div(cx)?;
-    msg.set_text_content(Some("Chat-Msg"));
     msg.add_class("chat_msg")?;
+    msg.set_text_content(Some(txt));
     Ok(msg)
 }
