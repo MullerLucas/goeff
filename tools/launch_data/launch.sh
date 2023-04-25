0548995f18cfbb99ff4fa8c1f9864cbf0e880ccd
helloki <helloki@mailbox.org>
2023-04-25 23:12:07 +0200

    feat (prod): add prod docker config

---
+++ tools/launch_data/launch.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+POSITIONAL_ARGS=()
+
+SH_DIR=$(dirname $0)
+ENVIRONMENT="dev"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -e|--environment)
+      ENVIRONMENT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+COMPOSE_FILE="$SH_DIR/docker-compose.${ENVIRONMENT}.yml"
+
+
+echo "launching hellmut container ..."
+echo "ENVIRONMENT: $ENVIRONMENT"
+echo "-------------------------------------------"
+
+echo "starting docker service..."
+sudo systemctl start docker || exit 1
+
+echo "starting docker containers..."
+sudo docker compose up --build --project-directory "$SH_DIR" -f "$COMPOSE_FILE"
+
+echo "returning to original dir..."
+cd "$orig_dir" || exit 1
